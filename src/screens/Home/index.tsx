099fb2b6249d2f5fb26c6635e52b5e9d493f73bc
DiegoAlexandres <diegoalexandre.me@gmail.com>
2024-02-14 20:19:01 -0300

    Estilos aplicados no componente Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,41 @@
-import { Text, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles'
 
 export function Home() {
+  
+  function hundleParticipantAdd() {
+    return(
+      console.log("Participante adicionado!")
+    );
+  }
+  
   return (
     <View style={styles.container}>
+      
       <Text style={styles.eventName}>
-        Hello
+        Nome do evento
       </Text>
+
       <Text style={styles.evetnDate}>
         Terça, 13 de Fevereiro de 2024
       </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor= "#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={hundleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+
+      </View>
+
+
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,17 +3,54 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#121214',
-      padding: 48,
+      backgroundColor: '#131016',
+      paddingTop: 75, 
+      paddingLeft: 24,
+      paddingRight: 24,
     },
   
     eventName: {
-        color: '#EB8A1D',
-        fontSize: 32,
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
     },
   
     evetnDate: {
-      color: '#FBA94C',
-      fontSize: 18,
+      color: '#6B6B6B',
+      fontSize: 16,
+      marginTop: 4,
+    },
+
+    input: {
+      flex: 1,
+      height: 56,
+      backgroundColor: "#1F1E25",
+      borderRadius: 4,
+      padding: 16,
+      color: "#fff",
+      fontSize: 16,
+    },
+
+    button: {
+      width: 56,
+      height: 56,
+      backgroundColor: "#31CF67",
+      borderRadius: 4,
+      alignItems: "center",
+      justifyContent: "center"
+    },
+
+    buttonText: {
+      color: "#fff",
+      fontSize: 24,
+    },
+
+    form: {
+      width: "100%",
+      flexDirection: "row",
+      marginTop: 34,
+      marginBottom: 42,
+      gap: 8,
     }
+
   })
